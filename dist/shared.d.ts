ae3362f29b8af1e15cfcad05344094f0dc7be174
Jordan Laramie <jordanlaramie@gmail.com>
2022-01-24 17:02:19 -0500

    build

--- dist/shared.d.ts
+++ dist/shared.d.ts
@@ -6,7 +6,7 @@ export declare const EVT_RENDER = "formik/render";
 export declare const EVT_SUBMIT = "formik/submit";
 export declare const EVT_ON_SUBMIT = "formik/on-submit";
 declare type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
-export declare type ConfigWithoutExtra<Values = any> = PartialBy<FormikConfig<Values> & {
+export declare type ConfigWithoutExtra<Values = any> = PartialBy<Omit<FormikConfig<Values>, 'initialValues'> & {
     castArgs?: boolean;
     initialValues?: Values;
 }, 'onSubmit' | 'castArgs'>;
